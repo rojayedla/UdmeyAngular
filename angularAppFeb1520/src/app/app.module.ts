df05e9e229ddedc8d720fe1471bb21b0e0e654d0
rojayedla <rojayedla05@gmail.com>
2020-02-15 21:15:45 +0530

    first commit

--- angularAppFeb1520/src/app/app.module.ts
+++ angularAppFeb1520/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import {MatMenuModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
