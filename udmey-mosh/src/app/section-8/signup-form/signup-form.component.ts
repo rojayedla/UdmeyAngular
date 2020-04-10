c3ee706be0e827d895b959c7cd7a320bd5f5f8e7
rojayedla <roja.yedla@gmail.com>
2020-04-10 19:48:02 +0530

    Custom validations

--- udmey-mosh/src/app/section-8/signup-form/signup-form.component.ts
+++ udmey-mosh/src/app/section-8/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from '../username.validators';
 
 @Component({
   selector: 'app-signup-form',
@@ -8,7 +9,12 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent implements OnInit {
   form = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    // username: new FormControl('', [
+    //   Validators.required,
+    //   Validators.minLength(3),
+    //   UsernameValidators.cannotContainSpace
+    // ]),
+    username: new FormControl('', Validators.required,  UsernameValidators.shoudBeUnique),
     password : new FormControl('', Validators.required)
   });
 
